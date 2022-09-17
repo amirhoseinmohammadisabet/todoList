3af1f6b5c19f6a105996f9b6e84f5ee900822c07
Amirhosein Mohammadisabet <78011140+amirhoseinmohammadisabet@users.noreply.github.com>
2022-09-17 20:16:20 +0430

    starting a to do list with java script

---
+++ script.js
@@ -0,0 +1,20 @@
+class model {
+    constructor() {
+
+    }
+}
+
+class view {
+    constructor() {
+
+    }
+}
+
+class controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+}
+
+const app = new controller(new Model(), new View())
